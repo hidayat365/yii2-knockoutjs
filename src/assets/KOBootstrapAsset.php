857e58748bf07d1e394a3b0aa0fd6f3f78a26202
Damian Dennis <damiandennis@gmail.com>
2015-01-11 11:06:54 +1000

    switch back to original knockout as pull request was merged.

--- src/assets/KOBootstrapAsset.php
+++ src/assets/KOBootstrapAsset.php
@@ -18,7 +18,7 @@ class KOBootstrapAsset extends AssetBundle
 
     public function init()
     {
-        $this->setSourcePath(__DIR__ . '/../../../../bower/knockout-bootstrap3-version/src');
+        $this->setSourcePath(__DIR__ . '/../../../../npm/knockout-bootstrap/src');
         $this->setupAssets('js', ['knockout-bootstrap']);
         parent::init();
     }
