54f326cf4cbef3c71c609d83ddfda6dcebeaf8be
Damian Dennis <damiandennis@gmail.com>
2015-04-19 09:10:55 +1000

    added missing file.

---
+++ src/assets/KOClassBindingProviderAsset.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damian
+ * Date: 30/12/14
+ * Time: 5:37 AM
+ */
+
+namespace damiandennis\knockoutjs;
+
+class KOClassBindingProviderAsset extends AssetBundle
+{
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\web\YiiAsset',
+        'damiandennis\knockoutjs\KnockoutAsset',
+    ];
+
+    public function init()
+    {
+        $this->setSourcePath('@bower/knockout-classBindingProvider/build');
+        $this->setupAssets('js', ['knockout-classBindingProvider']);
+        parent::init();
+    }
+}
